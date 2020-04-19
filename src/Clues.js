01d18dbb23bf4703c79868db0936f6a037f9469c
McRae <mpetrey@gmail.com>
2020-04-19 12:36:54 -0400

    Grid and answers.

---
+++ src/Clues.js
@@ -0,0 +1,91 @@
+import React from 'react';
+
+export const Across = () => (
+  <div className="game-info">
+    <p>Across</p>
+    <p>1. They follow celebs</p>
+    <p>5. What a bite might make you</p>
+    <p>10 Something you love to do outdoors</p>
+    <p>14. "The train will be _ _ 10 </p>
+    <p>15. _ Islands (European chain)</p>
+    <p>16. Australian export</p>
+    <p>17. Amateur</p>
+    <p>18. It goes in the heart</p>
+    <p>19. Late screenwriter Ephron</p>
+    <p>20. Your family's newest addition</p>
+    <p>22. Go-to game</p>
+    <p>23. Resident of Paris</p>
+    <p>24. Ordinary Joes</p>
+    <p>26. First half of a chocolate drink</p>
+    <p>27. Diaries</p>
+    <p>29. Neighbor of a dingo</p>
+    <p>32. UKR. was a former member</p>
+    <p>34. What a 23-across would call salt</p>
+    <p>35. A Czech, for instance</p>
+    <p>36. What bar regulars often do</p>
+    <p>39. Largest city in Kansas</p>
+    <p>41. Things gym rats work out</p>
+    <p>42. McRae's title</p>
+    <p>44. She played with Garcia in the '80s</p>
+    <p>45. French plural possessive</p>
+    <p>46. It often plays dead</p>
+    <p>48. It can be great</p>
+    <p>51. Nickname for 20-across</p>
+    <p>52. Put in something else</p>
+    <p>54. It has Phans</p>
+    <p>57. Pride of 31-down</p>
+    <p>59. Shower need</p>
+    <p>60. They can be shown to you</p>
+    <p>61. "Let 'em _ _ !"</p>
+    <p>62. Utah ski resort</p>
+    <p>63."Guilty" and "not guilty," for two</p>
+    <p>64. What you might have to take after a bad game, in slang</p>
+    <p>65. Someone taking the 54-down</p>
+    <p>66. The thick of things</p>
+    <p>67. Helps</p>
+  </div>
+);
+
+export const Down = () => (
+  <div>
+    <p>Down</p>
+
+    <p>1. What a singer never wants to be called</p>
+    <p>2. "As I was saying," casually</p>
+    <p>3. Belonging to a major film studio</p>
+    <p>4. It's bound to ruin a hike</p>
+    <p>5. In that case</p>
+    <p>6. Rat</p>
+    <p>7. Greek island residents</p>
+    <p>8. Cantaloupe cousin</p>
+    <p>9. So far</p>
+    <p>10. Former rival of Jay</p>
+    <p>11. What you need to cook a favorite dish</p>
+    <p>12. She played Matilda</p>
+    <p>13. A good thing to have, generally</p>
+    <p>21. Smallest</p>
+    <p>22. National tech tradeshow</p>
+    <p>25. Word before rum or Barbie</p>
+    <p>28.Abe Simpson, for example</p>
+    <p>29. She ruled</p>
+    <p>30. Need for a wrestler</p>
+    <p>31. Bennett's home</p>
+    <p>33. You can take it to the EU</p>
+    <p>35. Word before "bangs" or "wolf" in song</p>
+    <p>36. 'Treasure Island' author's initials</p>
+    <p>37. Dubai's home</p>
+    <p>38. Cauliflower cousin</p>
+    <p>40. Marlboro rival</p>
+    <p>43. Like some roads</p>
+    <p>46. Thing you might say after figuring out a hard crossword clue</p>
+    <p>47. Edit menu option</p>
+    <p>49. Chemically curled</p>
+    <p>50. The cars of the future, once</p>
+    <p>51. Where to watch Congress</p>
+    <p>53.Mazda offering</p>
+    <p>54. Juniors' requirement</p>
+    <p>55. Word sometimes after "black" or "worm"</p>
+    <p>56. "No dinner for me, thanks"</p>
+    <p>58 No. 2 60 Stat for a Grateful Dead record, perhaps</p>
+  </div>
+);
---
+++ src/index.js
@@ -0,0 +1,291 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import { Sample } from './sample.js';
+// import { Across, Down } from './Clues.js';
+
+const Square = ({ answer, numbered }) => {
+  return (
+    <div className={numbered ? 'numbered' : ''}>
+      <input className="square" type="text" maxLength="1" />
+    </div>
+  );
+};
+
+class Board extends React.Component {
+  renderSquare(answer, numbered = false) {
+    return <Square answer={answer} numbered={numbered} />;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="status">
+          Brians's Puzzle
+          <br />
+          Rachel Simon
+        </div>
+        <div id="wrapper">
+          {this.renderSquare('p', true)}
+          {this.renderSquare('a', true)}
+          {this.renderSquare('p', true)}
+          {this.renderSquare('s', true)}
+          <input className="square black" disabled />
+          {this.renderSquare('i', true)}
+          {this.renderSquare('t', true)}
+          {this.renderSquare('c', true)}
+          {this.renderSquare('h', true)}
+          {this.renderSquare('y', true)}
+          <input className="square black" disabled />
+          {this.renderSquare('c', true)}
+          {this.renderSquare('a', true)}
+          {this.renderSquare('m', true)}
+          {this.renderSquare('p', true)}
+          {/* 2nd row */}
+          {this.renderSquare('i', true)}
+          {this.renderSquare('n')}
+          {this.renderSquare('a')}
+          {this.renderSquare('t')}
+          <input className="square black" disabled />
+          {this.renderSquare('f', true)}
+          {this.renderSquare('a')}
+          {this.renderSquare('r')}
+          {this.renderSquare('o')}
+          {this.renderSquare('e')}
+          <input className="square black" disabled />
+          {this.renderSquare('o', true)}
+          {this.renderSquare('p')}
+          {this.renderSquare('a')}
+          {this.renderSquare('l')}
+          {/* 3rd row */}
+          {this.renderSquare('t', true)}
+          {this.renderSquare('y')}
+          {this.renderSquare('r')}
+          {this.renderSquare('o')}
+          <input className="square black" disabled />
+          {this.renderSquare('s', true)}
+          {this.renderSquare('t')}
+          {this.renderSquare('e')}
+          {this.renderSquare('n')}
+          {this.renderSquare('t')}
+          <input className="square black" disabled />
+          {this.renderSquare('n', true)}
+          {this.renderSquare('o')}
+          {this.renderSquare('r')}
+          {this.renderSquare('a')}
+          {/* 4th row */}
+          {this.renderSquare('c', true)}
+          {this.renderSquare('h')}
+          {this.renderSquare('a')}
+          {this.renderSquare('r')}
+          {this.renderSquare('l', true)}
+          {this.renderSquare('o')}
+          {this.renderSquare('t')}
+          {this.renderSquare('t')}
+          {this.renderSquare('e')}
+          <input className="square black" disabled />
+          {this.renderSquare('c', true)}
+          {this.renderSquare('a')}
+          {this.renderSquare('t')}
+          {this.renderSquare('a')}
+          {this.renderSquare('n')}
+          {/* 5th row */}
+          {this.renderSquare('h', true)}
+          {this.renderSquare('o')}
+          {this.renderSquare('m')}
+          {this.renderSquare('m')}
+          {this.renderSquare('e')}
+          <input className="square black" disabled />
+          {this.renderSquare('l', true)}
+          {this.renderSquare('a')}
+          {this.renderSquare('y')}
+          {this.renderSquare('m', true)}
+          {this.renderSquare('e')}
+          {this.renderSquare('n')}
+          <input className="square black" disabled />
+          <input className="square black" disabled />
+          <input className="square black" disabled />
+          {/* 6th row */}
+          {this.renderSquare('y', true)}
+          {this.renderSquare('o')}
+          {this.renderSquare('o')}
+          <input className="square black" disabled />
+          {this.renderSquare('a', true)}
+          {this.renderSquare('g', true)}
+          {this.renderSquare('e')}
+          {this.renderSquare('n')}
+          {this.renderSquare('d')}
+          {this.renderSquare('a')}
+          {this.renderSquare('s')}
+          <input className="square black" disabled />
+          {this.renderSquare('e', true)}
+          {this.renderSquare('m', true)}
+          {this.renderSquare('u', true)}
+          {/* 7th row */}
+          <input className="square black" disabled />
+          <input className="square black" disabled />
+          {this.renderSquare('u', true)}
+          {this.renderSquare('s', true)}
+          {this.renderSquare('s')}
+          {this.renderSquare('r')}
+          <input className="square black" disabled />
+          {this.renderSquare('s', true)}
+          {this.renderSquare('e')}
+          {this.renderSquare('l')}
+          <input className="square black" disabled />
+          {this.renderSquare('s', true)}
+          {this.renderSquare('l')}
+          {this.renderSquare('a')}
+          {this.renderSquare('v')}
+          {/* 8th row */}
+          {this.renderSquare('r', true)}
+          {this.renderSquare('u', true)}
+          {this.renderSquare('n')}
+          {this.renderSquare('a')}
+          {this.renderSquare('t')}
+          {this.renderSquare('a')}
+          {this.renderSquare('b', true)}
+          <input className="square black" disabled />
+          {this.renderSquare('w', true)}
+          {this.renderSquare('i')}
+          {this.renderSquare('c', true)}
+          {this.renderSquare('h')}
+          {this.renderSquare('i')}
+          {this.renderSquare('t')}
+          {this.renderSquare('a')}
+          {/* 9th row */}
+          {this.renderSquare('l', true)}
+          {this.renderSquare('a')}
+          {this.renderSquare('t')}
+          {this.renderSquare('s')}
+          <input className="square black" disabled />
+          {this.renderSquare('m', true)}
+          {this.renderSquare('r')}
+          {this.renderSquare('s', true)}
+          <input className="square black" disabled />
+          {this.renderSquare('b', true)}
+          {this.renderSquare('a')}
+          {this.renderSquare('e')}
+          {this.renderSquare('z')}
+          <input className="square black" disabled />
+          <input className="square black" disabled />
+          {/* 10th row */}
+          {this.renderSquare('s', true)}
+          {this.renderSquare('e')}
+          {this.renderSquare('s')}
+          <input className="square black" disabled />
+          {this.renderSquare('o', true)}
+          {this.renderSquare('p')}
+          {this.renderSquare('o')}
+          {this.renderSquare('s')}
+          {this.renderSquare('s', true)}
+          {this.renderSquare('u')}
+          {this.renderSquare('m')}
+          <input className="square black" disabled />
+          {this.renderSquare('a', true)}
+          {this.renderSquare('p', true)}
+          {this.renderSquare('e', true)}
+          {/* 11th row */}
+          <input className="square black" disabled />
+          <input className="square black" disabled />
+          <input className="square black" disabled />
+          {this.renderSquare('c', true)}
+          {this.renderSquare('h')}
+          {this.renderSquare('a')}
+          {this.renderSquare('c')}
+          {this.renderSquare('h')}
+          {this.renderSquare('a')}
+          <input className="square black" disabled />
+          {this.renderSquare('e', true)}
+          {this.renderSquare('m', true)}
+          {this.renderSquare('b')}
+          {this.renderSquare('e')}
+          {this.renderSquare('d')}
+          {/* 12th row */}
+          {this.renderSquare('p', true)}
+          {this.renderSquare('h', true)}
+          {this.renderSquare('i', true)}
+          {this.renderSquare('s')}
+          {this.renderSquare('h')}
+          <input className="square black" disabled />
+          {this.renderSquare('c', true)}
+          {this.renderSquare('a')}
+          {this.renderSquare('v')}
+          {this.renderSquare('a', true)}
+          {this.renderSquare('l')}
+          {this.renderSquare('i')}
+          {this.renderSquare('e')}
+          {this.renderSquare('r')}
+          {this.renderSquare('s')}
+          {/* 13th row */}
+          {this.renderSquare('s', true)}
+          {this.renderSquare('o')}
+          {this.renderSquare('a')}
+          {this.renderSquare('p')}
+          <input className="square black" disabled />
+          {this.renderSquare('r', true)}
+          {this.renderSquare('o')}
+          {this.renderSquare('p')}
+          {this.renderSquare('e')}
+          {this.renderSquare('s')}
+          <input className="square black" disabled />
+          {this.renderSquare('a', true)}
+          {this.renderSquare('t')}
+          {this.renderSquare('m')}
+          {this.renderSquare('e')}
+          {/* 14th row */}
+          {this.renderSquare('a', true)}
+          {this.renderSquare('l')}
+          {this.renderSquare('t')}
+          {this.renderSquare('a')}
+          <input className="square black" disabled />
+          {this.renderSquare('p', true)}
+          {this.renderSquare('l')}
+          {this.renderSquare('e')}
+          {this.renderSquare('a')}
+          {this.renderSquare('s')}
+          <input className="square black" disabled />
+          {this.renderSquare('t', true)}
+          {this.renderSquare('r')}
+          {this.renderSquare('u')}
+          {this.renderSquare('e')}
+          {/* 15th row */}
+          {this.renderSquare('t', true)}
+          {this.renderSquare('e')}
+          {this.renderSquare('e')}
+          {this.renderSquare('n')}
+          <input className="square black" disabled />
+          {this.renderSquare('m', true)}
+          {this.renderSquare('i')}
+          {this.renderSquare('d')}
+          {this.renderSquare('s')}
+          {this.renderSquare('t')}
+          <input className="square black" disabled />
+          {this.renderSquare('a', true)}
+          {this.renderSquare('i')}
+          {this.renderSquare('d')}
+          {this.renderSquare('s')}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        {/* <Sample /> */}
+        {/* <Across />
+        <Down /> */}
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById('root'));
